3798aac1e202719808eab1dbe4167c1acd8bb7d9
fred813jay <51696109+fred813jay@users.noreply.github.com>
2019-06-14 10:01:33 +0800

    pug sass

---
+++ src/script.js
@@ -0,0 +1,52 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    userinput: "",
+    user: "",
+    FinishedTasks: "",
+    taskinput: "",
+    tasks: [
+      { id: 0, title: "enjoy the day", status: false },
+      { id: 1, title: "Everything i do i do it big", status: false}
+    ]
+  },
+  methods:{
+    adduser: function(){
+      this.user = this.userinput
+    },
+    addtask: function(taskinput){
+      if (taskinput.length != 0){
+        this.tasks.push({
+          id: this.tasks.length,
+          title: this.taskinput,
+          status: false
+        });
+      }
+      this.taskinput = ""
+    },
+    CheckStatus: function(task){
+      this.tasks[task.id].status = !this.tasks[task.id].status;
+    },
+    remove: function(task){
+      var r= confirm("Are you sure?")
+      if (r == true){
+        this.tasks.splice((task.id),1)
+        for (let i=0 ; i<this.tasks.length ; i++){
+          this.tasks[i].id = i
+        }
+      }
+    }
+  },
+  computed:{
+    unfinished(){
+      return this.tasks.filter(function(task) {
+        return !task.status;
+      });
+    },
+    finsined(){
+       return this.tasks.filter(function(task) {
+        return task.status;
+      });     
+    }
+  }
+})
